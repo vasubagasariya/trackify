60d1562ad60915d3c90c95e178d5cf3d5e82ac7a
Vasu Bagasariya <vasubagasariya2@gmail.com>
2026-01-09 08:36:58 +0530

    css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('adminlte/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     {{-- <script src="/path/to/jquery.min.js"></script>
